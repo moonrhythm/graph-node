892a923411c81ac9e9423f529d5b489d3dbe3f2f
Thanatat Tamtan <acoshift@gmail.com>
2021-12-16 17:31:13 +0700

    update build script

--- docker/build.sh
+++ docker/build.sh
@@ -14,13 +14,23 @@ then
     REPO_NAME="Checkout of $(git remote get-url origin) at $(git describe --dirty)"
     BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 fi
-for stage in graph-node-build graph-node graph-node-debug
-do
-    $docker build --target $stage \
-            --build-arg "COMMIT_SHA=$COMMIT_SHA" \
-            --build-arg "REPO_NAME=$REPO_NAME" \
-            --build-arg "BRANCH_NAME=$BRANCH_NAME" \
-            --build-arg "TAG_NAME=$TAG_NAME" \
-            -t $stage \
-            -f docker/Dockerfile .
-done
+#for stage in graph-node-build graph-node graph-node-debug
+#do
+#    $docker build --target $stage \
+#            --build-arg "COMMIT_SHA=$COMMIT_SHA" \
+#            --build-arg "REPO_NAME=$REPO_NAME" \
+#            --build-arg "BRANCH_NAME=$BRANCH_NAME" \
+#            --build-arg "TAG_NAME=$TAG_NAME" \
+#            -t $stage \
+#            -f docker/Dockerfile .
+#done
+
+buildctl build \
+        --frontend dockerfile.v0 \
+        --local dockerfile=docker \
+        --local context=. \
+        --opt target=graph-node \
+        --opt build-arg=COMMIT_SHA=$COMMIT_SHA \
+        --opt build-arg=REPO_NAME=$REPO_NAME \
+        --opt build-arg=BRANCH_NAME=$BRANCH_NAME \
+        --output type=image,name=gcr.io/moonrhythm-containers/graph-node:$BRANCH_NAME,push=true
